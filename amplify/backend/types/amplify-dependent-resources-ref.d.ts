2fd02e7cb2d3825dab333765ebd4c536cad263ab
Zach Moore <sluggermoore@gmail.com>
2023-04-26 10:42:24 -0400

    amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "zachnicole": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
